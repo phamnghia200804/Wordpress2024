0c146e61b08d3714de8df4e2592ce6fd071e1b29
phamnghia521 <pxn200804@gmail.com>
2024-10-18 16:10:22 +0700

    hoàn thành footer

--- wp-content/themes/twentytwenty/index.php
+++ wp-content/themes/twentytwenty/index.php
@@ -21,10 +21,10 @@ get_header();
 
 	<?php
 
-	$archive_title    = '';
+	$archive_title = '';
 	$archive_subtitle = '';
-
-	if ( is_search() ) {
+    $archive_image='';
+	if (is_search()) {
 		/**
 		 * @global WP_Query $wp_query WordPress Query object.
 		 */
@@ -32,11 +32,11 @@ get_header();
 
 		$archive_title = sprintf(
 			'%1$s %2$s',
-			'<span class="color-accent">' . __( 'Search:', 'twentytwenty' ) . '</span>',
+			'<span class="color-accent">' . __('Search:', 'twentytwenty') . '</span>',
 			'&ldquo;' . get_search_query() . '&rdquo;'
 		);
 
-		if ( $wp_query->found_posts ) {
+		if ($wp_query->found_posts) {
 			$archive_subtitle = sprintf(
 				/* translators: %s: Number of search results. */
 				_n(
@@ -45,31 +45,35 @@ get_header();
 					$wp_query->found_posts,
 					'twentytwenty'
 				),
-				number_format_i18n( $wp_query->found_posts )
+				number_format_i18n($wp_query->found_posts)
 			);
 		} else {
-			$archive_subtitle = __( 'We could not find any results for your search. You can give it another try through the search form below.', 'twentytwenty' );
+			$archive_subtitle = __('We could not find any results for your search. You can give it another try through the search form below.', 'twentytwenty');
 		}
-	} elseif ( is_archive() && ! have_posts() ) {
-		$archive_title = __( 'Nothing Found', 'twentytwenty' );
-	} elseif ( ! is_home() ) {
-		$archive_title    = get_the_archive_title();
+	} elseif (is_archive() && !have_posts()) {
+		$archive_title = __('Nothing Found', 'twentytwenty');
+	} elseif (!is_home()) {
+		$archive_title = get_the_archive_title();
 		$archive_subtitle = get_the_archive_description();
 	}
 
-	if ( $archive_title || $archive_subtitle ) {
+	if ($archive_title || $archive_subtitle) {
 		?>
 
 		<header class="archive-header has-text-align-center header-footer-group">
 
 			<div class="archive-header-inner section-inner medium">
-
-				<?php if ( $archive_title ) { ?>
-					<h1 class="archive-title"><?php echo wp_kses_post( $archive_title ); ?></h1>
+				<?php if ($archive_image) { ?>
+					<h1 class="archive-image"><?php echo wp_kses_post($archive_image); ?></h1>
+				<?php } ?>
+				<?php if ($archive_title) { ?>
+					<h1 class="archive-title"><?php echo wp_kses_post($archive_title); ?></h1>
 				<?php } ?>
 
-				<?php if ( $archive_subtitle ) { ?>
-					<div class="archive-subtitle section-inner thin max-percentage intro-text"><?php echo wp_kses_post( wpautop( $archive_subtitle ) ); ?></div>
+				<?php if ($archive_subtitle) { ?>
+					<div class="archive-subtitle section-inner thin max-percentage intro-text">
+						<?php echo wp_kses_post(wpautop($archive_subtitle)); ?>
+					</div>
 				<?php } ?>
 
 			</div><!-- .archive-header-inner -->
@@ -79,21 +83,20 @@ get_header();
 		<?php
 	}
 
-	if ( have_posts() ) {
+	if (have_posts()) {
 
 		$i = 0;
-
-		while ( have_posts() ) {
+		while (have_posts()) {
 			++$i;
-			if ( $i > 1 ) {
+			if ($i > 1) {
 				echo '<hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" />';
 			}
 			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			get_template_part('template-parts/content', get_post_type());
 
 		}
-	} elseif ( is_search() ) {
+	} elseif (is_search()) {
 		?>
 
 		<div class="no-search-results-form section-inner thin">
@@ -101,7 +104,7 @@ get_header();
 			<?php
 			get_search_form(
 				array(
-					'aria_label' => __( 'search again', 'twentytwenty' ),
+					'aria_label' => __('search again', 'twentytwenty'),
 				)
 			);
 			?>
@@ -112,11 +115,11 @@ get_header();
 	}
 	?>
 
-	<?php get_template_part( 'template-parts/pagination' ); ?>
+	<?php get_template_part('template-parts/pagination'); ?>
 
 </main><!-- #site-content -->
 
-<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+<?php get_template_part('template-parts/footer-menus-widgets'); ?>
 
 <?php
 get_footer();
